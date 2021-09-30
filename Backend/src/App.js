42efbae6ea605e52261f1da05eba16f3d43bb9fb
rodriggoshell <rodriggoshell@gmail.com>
2021-09-29 23:10:09 -0300

    Desenvolvendo front e testando integração com back

---
+++ Backend/src/App.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const socket = require('socket.io');
+const cors = require('cors');
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type, Authorization');
+    app.use(cors());
+    next();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Bem-Vindos');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const server = app.listen(8080, () => {
+    console.log('servidor conectado');
+});
+
+io = socket(server, {cors:{origin:"*"}});
+io.on('conection', (socket) => {
+    console.log(socket.id);
+});
---
+++ front-end/src/App.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+
+//socet.io
+let socket;
+
+function App() {
+    const ENDPOINT = 'http://localhost:8080/';
+    
+    const [logado] = useState(false);
+    const [name, setName] = useState("");
+    const [sala, setSala] = useState("");
+    
+    useEffect(()=>{ 
+      socket = socketIOClient(ENDPOINT);   
+    }, []);
+    
+    const conectarSala = (e) => {
+      e.preventDefault();
+      console.log('Acessou a sala de ' + sala + ' com o usuario ' + name);
+     }
+    
+    return (
+      <div>
+        <h1>Bem-Vindo(a) ao ChatApp</h1>
+        <form>
+          <div>
+            <label>Nome:</label>
+            <input type="text" placeholder="Nome" name="nome" value={name} onChange={(e)=> setName(e.target.value)} />
+          </div>
+          {/*<div>
+            <label>Sala:</label>
+            <input type="text" placeholder="Sala" value={sala} onChange={(e)=> setSala(e.target.value)} />
+          </div>*/}
+          <div>  
+            <select name="sala" value={sala} onChange={e =>setSala(e.target.value)}>
+              <option value="">Selecione</option>
+              <option value="1">Saúde</option>
+              <option value="2">Cultura</option>
+              <option value="3">Esporte</option>
+              <option value="4">Educação</option>
+            </select> 
+          </div>  
+          <button onClick={conectarSala}>Acessar</button>
+        </form>
+        {! logado ? "Você precisa Está Logado" : "Logado"}
+      </div>
+    );
+}
+
+export default App;
